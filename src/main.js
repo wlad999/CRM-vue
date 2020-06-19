9b08625cf35dd896d25766a9848b2e0f693cab4e
wlad999 <wlad25888@gmail.com>
2020-06-19 16:30:24 +0300

    Login form validations

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
+import Vuilidate from 'vuelidate'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
+import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min.js'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuilidate)
 Vue.filter('date', dateFilter)
 new Vue({
   router,
