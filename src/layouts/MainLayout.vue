1e10fe86e4cdde0f87a48233eb1eca28cfe8f131
wlad999 <wlad25888@gmail.com>
2020-06-23 18:46:09 +0300

    fix first data add loader

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,19 +1,22 @@
 <template>
-  <div class="app-main-layout">
+  <div>
+    <Loader v-if="loading" />
+    <div class="app-main-layout" v-else>
 
-    <NavBar @click="isOpen = !isOpen"/>
-    <SideBar v-model="isOpen"/>
+      <NavBar @click="isOpen = !isOpen"/>
+      <SideBar v-model="isOpen"/>
 
-    <main class="app-content" :class="{full:!isOpen}">
-      <div class="app-page">
-        <router-view />
+      <main class="app-content" :class="{full:!isOpen}">
+        <div class="app-page">
+          <router-view />
+        </div>
+      </main>
+
+      <div class="fixed-action-btn">
+        <router-link class="btn-floating btn-large blue" to="/record">
+          <i class="large material-icons">add</i>
+        </router-link>
       </div>
-    </main>
-
-    <div class="fixed-action-btn">
-      <router-link class="btn-floating btn-large blue" to="/record">
-        <i class="large material-icons">add</i>
-      </router-link>
     </div>
   </div>
 </template>
@@ -25,13 +28,15 @@ import NavBar from '@/components/app/NavBar'
 export default {
   name: 'main-layout',
   data: () => ({
-    isOpen: true
+    isOpen: true,
+    loading: true
 
   }),
   async mounted () {
     if (!Object.keys(this.$store.getters.info).length) {
       await this.$store.dispatch('fetchInfo')
     }
+    this.loading = false
   },
   components: {
     SideBar,
