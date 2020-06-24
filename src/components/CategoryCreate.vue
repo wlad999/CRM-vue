3bb3efdb053ba3e8df20d02acb566be7b1542022
wlad999 <wlad25888@gmail.com>
2020-06-24 14:22:06 +0300

    create CategoryCreate & made validations

---
+++ src/components/CategoryCreate.vue
@@ -0,0 +1,85 @@
+<template lang="">
+  <div class="col s12 m6">
+    <div>
+      <div class="page-subtitle">
+        <h4>Создать</h4>
+      </div>
+
+      <form @submit.prevent="submitHendler">
+        <div class="input-field">
+          <!-- <input
+            id="name"
+            type="text"
+            v-model="title"
+            :class="{invalid: $v.title.$dirty && !$v.title.required}"
+          > -->
+          <input
+            id="name"
+            type="text"
+            v-model="title"
+            :class="{invalid: !$v.title.required}"
+          >
+          <label for="name">Название</label>
+          <span
+            v-if="!$v.title.required"
+            class="helper-text invalid"
+          >
+            Введите название категории
+          </span>
+        </div>
+
+        <div class="input-field">
+          <!-- <input
+          id="limit"
+          type="number"
+          v-model="limit"
+          :class="{invalid: $v.limit.$dirty && !$v.limit.minValue}"
+          > -->
+          <input
+          id="limit"
+          type="number"
+          v-model="limit"
+          :class="{invalid: !$v.limit.minValue}"
+          >
+          <label for="limit">Лимит</label>
+          <span
+            v-if="!$v.limit.minValue"
+            class="helper-text invalid"
+          >
+          Минимальное значение {{$v.limit.$params.minValue.min}}
+          </span>
+        </div>
+
+        <button class="btn waves-effect waves-light" type="submit">
+          Создать
+          <i class="material-icons right">send</i>
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { required, minValue } from 'vuelidate/lib/validators'
+
+export default {
+  data: () => ({
+    title: '',
+    limit: 100
+  }),
+  validations: {
+    title: { required },
+    limit: { minValue: minValue(100) }
+  },
+  mounted () {
+    window.M.updateTextFields()
+  },
+  methods: {
+    submitHendler () {
+      if (this.$v.invalid) {
+        this.$v.touch()
+      }
+    }
+  }
+}
+</script>
