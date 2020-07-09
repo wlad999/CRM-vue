084e73eb5cc61c9d811172d891192ff5a94d53b6
wlad999 <wlad25888@gmail.com>
2020-07-09 16:48:11 +0300

    Detail page

--- src/store/category.js
+++ src/store/category.js
@@ -35,6 +35,18 @@ export default {
         throw error
       }
 
-    }
+    },
+    async fetchCategoryById ({ commit, dispatch }, id) {
+      try {
+        const uid = await dispatch('getUid')
+        const category = (await firebase.database().ref(`/users/${uid}/categories`).child(id).once('value')).val()||{}
+        return {...category, id}
+
+      } catch (error) {
+        commit('setError', error)
+        throw error
+      }
+
+    },
   }
 }
